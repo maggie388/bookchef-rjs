4e82316e2c889989344eb380f550ec7ba6f8692c
maggie <rodriguesmargarida@hotmail.com>
2020-12-06 15:14:59 -0500

    Access file name after upload, install Google Vision dependency

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
 
 // MULTER CONFIG
 const multer = require('multer');
@@ -9,13 +11,14 @@ const storage = multer.diskStorage({
         cb(null, './uploads')
     }, 
     filename: function (_req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
+        cb(null, Date.now() + '-' + file.originalname);
     }
 });
 
 const upload = multer({ storage: storage });
 
-router.post('/', upload.single('file'), (_req, res) => {
+router.post('/', upload.single('file'), (req, res) => {
+    console.log(req.file.filename);
     res.status(200).send("success!");
 });
 
