4141c57389e15deefafe1a410e195d3db9d4195c
maggie <rodriguesmargarida@hotmail.com>
2020-12-07 17:10:30 -0500

    Create route to post a new recipe

--- client/src/components/RecipeListItem/RecipeListItem.js
+++ client/src/components/RecipeListItem/RecipeListItem.js
@@ -4,7 +4,7 @@ import './RecipeListItem.scss';
 const RecipeListItem = ({ recipe }) => {
     // other avaiable value to deconstruct: id, image, category, ingredients, instructions
     const { title, book, page } = recipe;
-    
+
     return (
         <div className='recipe-list-item'>
             <div className='recipe-list-item__pic-div'>
@@ -12,8 +12,8 @@ const RecipeListItem = ({ recipe }) => {
             </div>
             <div className='recipe-list-item__details-div'>
                 <h2 className='recipe-list-item__title'>{title}</h2>
-                <p className='recipe-list-item__book'>Book:{book}</p>
-                <p className='recipe-list-item__page'>Page:{page}</p>
+                <p className='recipe-list-item__book'>Book: {book}</p>
+                <p className='recipe-list-item__page'>Page: {page}</p>
             </div>
         </div>
     );
--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -3,11 +3,28 @@ const router = express.Router();
 
 const Recipes = require('../models/recipes');
 
-router.get('/', (req,res) => {
+router.get('/', (_req,res) => {
     Recipes
         .fetchAll()
         .then(recipes => {
             res.status(200).json(recipes);
+        });
+});
+
+router.post('/', (req, res) => {
+    new Recipes({
+        user_id: req.body.userId,
+        title: req.body.title,
+        book: req.body.book,
+        page: req.body.page,
+        category: req.body.category,
+        ingredients: JSON.stringify(req.body.ingredients),
+        instructions: JSON.stringify(req.body.instructions),
+        image: ''
+    })
+        .save()
+        .then(newRecipe => {
+            res.status(201).json({newRecipe});
         });
 });
 
