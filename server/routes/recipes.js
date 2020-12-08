5e3f7fdb35fc6ebc4331f525f0fcc422d594199c
maggie <rodriguesmargarida@hotmail.com>
2020-12-08 16:14:27 -0500

    Add route for updating a recipe

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -27,14 +27,39 @@ router.post('/', (req, res) => {
         book: req.body.book,
         page: req.body.page,
         category: req.body.category,
-        ingredients: JSON.stringify(req.body.ingredients),
-        instructions: JSON.stringify(req.body.instructions),
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
         image: ''
     })
         .save()
         .then(newRecipe => {
             res.status(201).json({newRecipe});
         });
+});
+
+router.put('/:recipeId', (req, res) => {
+    Recipes
+        .where({ id: req.params.recipeId })
+        .fetch()
+        .then(recipe => {
+            const { userId, title, book, page, category, ingredients, instructions, image } = req.body;
+            recipe
+                .save({
+                    user_id: userId ? userId : recipe.user_id,
+                    title: title ? title : recipe.title,
+                    book: book ? book : recipe.book,
+                    page: page ? page : recipe.page,
+                    category: category ? category : recipe.category,
+                    ingredients: ingredients ? ingredients : recipe.ingredients,
+                    instructions: instructions ? instructions : recipe.instructions,
+                    image: image ? image : recipe.image
+                })
+                .then(newRecipe => {
+                    res.status(201).json(newRecipe)
+                })
+                .catch(error => console.log(error));
+        })
+        .catch(error => console.log(error))
 });
 
 module.exports = router;
