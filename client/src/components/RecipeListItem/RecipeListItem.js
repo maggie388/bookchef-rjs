077e017bf65db685d67bda4a3a4a2c81d207bf55
maggie <rodriguesmargarida@hotmail.com>
2020-12-08 09:42:45 -0500

    Add styling to recipe page, create links, and add request skeleton

--- client/src/components/RecipeListItem/RecipeListItem.js
+++ client/src/components/RecipeListItem/RecipeListItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './RecipeListItem.scss';
 
 const RecipeListItem = ({ recipe }) => {
@@ -6,16 +7,18 @@ const RecipeListItem = ({ recipe }) => {
     const { title, book, page } = recipe;
 
     return (
-        <div className='recipe-list-item'>
-            <div className='recipe-list-item__pic-div'>
-                <img className='recipe-list-item__pic' src='https://via.placeholder.com/100' alt='' />
+        <Link to={`/recipe/${recipe.id}`}>
+            <div className='recipe-list-item'>
+                <div className='recipe-list-item__pic-div'>
+                    <img className='recipe-list-item__pic' src='https://via.placeholder.com/100' alt='' />
+                </div>
+                <div className='recipe-list-item__details-div'>
+                    <h2 className='recipe-list-item__title'>{title}</h2>
+                    <p className='recipe-list-item__book'>Book: {book}</p>
+                    <p className='recipe-list-item__page'>Page: {page}</p>
+                </div>
             </div>
-            <div className='recipe-list-item__details-div'>
-                <h2 className='recipe-list-item__title'>{title}</h2>
-                <p className='recipe-list-item__book'>Book: {book}</p>
-                <p className='recipe-list-item__page'>Page: {page}</p>
-            </div>
-        </div>
+        </Link>
     );
 };
 
--- client/src/pages/Recipe/Recipe.js
+++ client/src/pages/Recipe/Recipe.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './Recipe.scss';
 
 // ASSETS
 import backIcon from '../../assets/icons/arrow-back-sharp.svg';
+import editIcon from '../../assets/icons/pencil-sharp.svg';
+
+// VARIABLES
+const API_URL = process.env.REACT_APP_API_URL;
 
 class Recipe extends Component {
     state = {
-        recipeId: '',
+        isLoading: true,
+        recipeId: this.props.match.params.recipeId,
         image: '',
         title: '',
         book: '',
@@ -16,7 +23,23 @@ class Recipe extends Component {
         instructions: ''
     }
 
+    getSingleRecipe = () => {
+        axios.get(API_URL + `/recipes/${this.props.recipe.id}`)
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => console.log(error));
+    }
+
+    componentDidMount = () => {
+
+    }
+
     render() {
+        if (this.state.isLoading) {
+            return "Loading...";
+        }
+
         return (
             <div className='recipe'>
                 <div className='recipe__top'>
@@ -24,25 +47,32 @@ class Recipe extends Component {
                 </div>
                 <div className='recipe__bottom'>
                     <div className='recipe__title-div'>
-                        <img className='recipe__go-back' src={backIcon} alt='' />
+                        <Link to='/'>
+                            <img className='recipe__go-back' src={backIcon} alt='Go Back' />
+                        </Link>
                         <h1 className='recipe__title'>Garlic Mashed Cauliflower</h1>
+                        <Link to='/'>
+                            <img className='recipe__edit' src={editIcon} alt='Edit' />
+                        </Link>
                     </div>
-                    <p>Love and Lemons, page 25</p>
-                    <p>Side Dish</p>
-                    <h2>Ingredients</h2>
-                    <ul>
-                        <li>1 medium head cauliflower, 2 pounds, chopped</li>
-                        <li>2 tbsp melted butter or vegan butter</li>
-                        <li>7 cloves roasted garlic</li>
-                        <li>1/8 to 1/4 tsp Dijon mustard</li> <li>1/4 to 1/2 tsp sea salt</li>
-                        <li>Freshly ground black pepper</li>
-                        <li>Chives, for garnish, optional</li>
-                    </ul>
-                    <h2>Instructions</h2>
-                    <ul>
-                        <li>Bring a large pot of salted water to a boil. Add the cauliflower and boil until knife-tender, about 10 minutes. Drain and transfer to a food processor.</li>
-                        <li>Puree the cauliflower with the butter, garlic, mustard, salt, and pepper. Season to taste and garnish with chives, if desired.</li>
-                    </ul>
+                    <div className='recipe__details-div'>
+                        <h3 className='recipe__category'>Side Dish</h3>
+                        <p className='recipe__source'> Love and Lemons Everyday, page 25</p>
+                        <h2 className='recipe__subtitle'>Ingredients</h2>
+                        <ul className='recipe__list recipe__list--ingredients'>
+                            <li className='recipe__list-item'>1 medium head cauliflower, 2 pounds, chopped</li>
+                            <li className='recipe__list-item'>2 tbsp melted butter or vegan butter</li>
+                            <li className='recipe__list-item'>7 cloves roasted garlic</li>
+                            <li className='recipe__list-item'>1/8 to 1/4 tsp Dijon mustard</li> <li>1/4 to 1/2 tsp sea salt</li>
+                            <li className='recipe__list-item'>Freshly ground black pepper</li>
+                            <li className='recipe__list-item'>Chives, for garnish, optional</li>
+                        </ul>
+                        <h2 className='recipe__subtitle'>Instructions</h2>
+                        <ul className='recipe__list recipe__list--instructions'>
+                            <li  className='recipe__list-item'>Bring a large pot of salted water to a boil. Add the cauliflower and boil until knife-tender, about 10 minutes. Drain and transfer to a food processor.</li>
+                            <li  className='recipe__list-item'>Puree the cauliflower with the butter, garlic, mustard, salt, and pepper. Season to taste and garnish with chives, if desired.</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         );
