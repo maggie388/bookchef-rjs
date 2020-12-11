88c9e9e4044fa2f8d59b0990d474c5c4dd9b7118
maggie <rodriguesmargarida@hotmail.com>
2020-12-11 12:41:08 -0500

    Create migration and seed data for notes table

--- server/seeds/bookchef_seed.js
+++ server/seeds/bookchef_seed.js
@@ -1,5 +1,6 @@
 const usersData = require('../seed_data/users');
 const recipesData = require('../seed_data/recipes');
+const notesData = require('../seed_data/notes');
 
 exports.seed = function(knex) {
   return knex('users')
@@ -14,12 +15,27 @@ exports.seed = function(knex) {
           return knex('users').pluck('id');
         })
         .then(userIds => {
-          const recipeDataWithUserId = recipesData.map((recipe) => {
+          const recipeDataWithUserId = recipesData.map(recipe => {
             const randomUserId = Math.floor(Math.random() * userIds.length);
             recipe.user_id = userIds[randomUserId];
             return recipe;
           });
           return knex('recipes').insert(recipeDataWithUserId);
         })
+        .then(() => {
+          return knex('notes')
+            .del()
+            .then(() => {
+              return knex('recipes').pluck('id');
+            })
+            .then(recipeIds => {
+              const notesDataWithRecipeId = notesData.map(note => {
+                const randomRecipeId = Math.floor(Math.random() * recipeIds.length);
+                note.recipe_id = recipeIds[randomRecipeId];
+                return note;
+              });
+              return knex('notes').insert(notesDataWithRecipeId);
+            })
+        });
     });
 };
