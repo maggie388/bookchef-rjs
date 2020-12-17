6376f76c8c4b908ed4904d82849a79e07e1f08a2
maggie <rodriguesmargarida@hotmail.com>
2020-12-16 20:30:39 -0500

    Commit non-sensitive environment variables

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,6 +1,3 @@
-// Update with your config settings.
-// "http://localhost:3306"
-
 module.exports = {
   client: 'mysql',
   connection: {
@@ -10,44 +7,4 @@ module.exports = {
     database: 'bookchef',
     charset: 'utf8'
   }
-
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   }
-  // },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
 };
