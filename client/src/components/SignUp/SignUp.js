6624f8fa08f4cd87c2f61b42ffd1b0a2ef020d40
maggie388 <rodriguesmargarida@hotmail.com>
2021-01-05 10:37:46 -0500

    Updated Sign Up Page inputs, added checklist for secure password requriements, add password strength checker

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import './SignUp.scss';
 // import axiosInstance from '../../utils/axios';
+import PasswordStrengthBar from 'react-password-strength-bar';
 
 // ASSETS
 import alertIcon from '../../assets/icons/alert.svg';
+import xIcon from '../../assets/icons/close-circle.svg';
+import checkIcon from '../../assets/icons/check-circle.svg';
 
 class SignUp extends Component {
     state = {
-        username: '',
+        name: '',
         email:'',
-        password: '',
-        confirmPassword: '', 
-        usernameError: true,
         emailError: true,
-        emailErrorMessage: 'Did you mean ...'
+        emailErrorMessage: 'Did you mean ...',
+        password: '',
+        passwordChecklistIcons: {
+            length: xIcon, 
+            lowercase: xIcon, 
+            uppercase: xIcon,
+            specialChar: xIcon,
+            number: xIcon
+        }
     }
 
     handleChange = (e) => {
@@ -23,11 +31,85 @@ class SignUp extends Component {
         });
     }
 
-    checkIfEmpty = () => {
-
+    handlePasswordChange = (e) => {
+        e.preventDefault();
+        this.setState({
+            [e.target.name]: e.target.value
+        }, () => {
+            this.passwordMeetsRequirements(this.state.password)
+        });
     }
 
-    isUsernameTaken = () => {
+    passwordMeetsRequirements(password) {
+        let passwordChecklistIcons = this.state.passwordChecklistIcons;
+
+        // check for minimum length
+        if (password.length > 7) {
+            passwordChecklistIcons.length = checkIcon;
+            this.setState({
+                passwordChecklistIcons
+            })
+        } else {
+            passwordChecklistIcons.length = xIcon;
+            this.setState({
+                passwordChecklistIcons
+            })
+        }
+
+        // check for a lowercase letter
+        if (password.match(/[a-z]/)) {
+            passwordChecklistIcons.lowercase = checkIcon;
+            this.setState({
+                passwordChecklistIcons
+            })
+        } else {
+            passwordChecklistIcons.lowercase = xIcon;
+            this.setState({
+                passwordChecklistIcons
+            })
+        }
+
+        // check for an uppercase letter
+        if (password.match(/[A-Z]/)) {
+            passwordChecklistIcons.uppercase = checkIcon;
+            this.setState({
+                passwordChecklistIcons
+            })
+        } else {
+            passwordChecklistIcons.uppercase = xIcon;
+            this.setState({
+                passwordChecklistIcons
+            })
+        }
+
+        // check for a special character
+        if (password.match(/\W|_/)) {
+            passwordChecklistIcons.specialChar = checkIcon;
+            this.setState({
+                passwordChecklistIcons
+            })
+        } else {
+            passwordChecklistIcons.specialChar = xIcon;
+            this.setState({
+                passwordChecklistIcons
+            })
+        }
+
+        // check for a special character
+        if (password.match(/\d/)) {
+            passwordChecklistIcons.number = checkIcon;
+            this.setState({
+                passwordChecklistIcons
+            })
+        } else {
+            passwordChecklistIcons.number = xIcon;
+            this.setState({
+                passwordChecklistIcons
+            })
+        }
+    }
+
+    checkIfEmpty = () => {
 
     }
 
@@ -66,21 +148,23 @@ class SignUp extends Component {
             )
         }
 
+        const { passwordChecklistIcons: icons } = this.state;
+
         return (
             <main className='sign-up'>
                 <form className='sign-up__form' onSubmit={this.handleSubmit}>
                     {/* USERNAME INPUT */}
                     <label 
                         className='sign-up__label' 
-                        htmlFor='username'>
-                            Username
+                        htmlFor='Name'>
+                            Name
                     </label>
                     <input 
                         className='sign-up__input' 
                         type='text' 
-                        name='username' 
-                        id='username' 
-                        value={this.state.username}
+                        name='name' 
+                        id='name' 
+                        value={this.state.name}
                         onChange={this.handleChange}
                     />
                     <div className='sign-up__error'>
@@ -115,14 +199,30 @@ class SignUp extends Component {
                         name='password' 
                         id='password' 
                         value={this.state.password}
-                        onChange={this.handleChange}
+                        onChange={this.handlePasswordChange}
                     />
-                    <ul className='sign-up__password-requirements'>
-                        <li>One lowercase character</li>
-                        <li>One uppercase character</li>
-                        <li>One special character</li>
-                        <li>8 characters minimim</li>
-                        <li>One number</li>
+                    <PasswordStrengthBar password={this.state.password} minLength='8'/>
+                    <ul className='sign-up__password-checklist'>
+                        <li className='sign-up__checklist-item'>
+                            <img className='sign-up__checklist-icon' src={icons.length} alt='' />
+                            <span>8 characters minimim</span>
+                        </li>
+                        <li className='sign-up__checklist-item'>
+                            <img className='sign-up__checklist-icon' src={icons.lowercase} alt='' />
+                            <span>One lowercase character</span>
+                        </li>
+                        <li className='sign-up__checklist-item'>
+                            <img className='sign-up__checklist-icon' src={icons.uppercase} alt='' />
+                            <span>One uppercase character</span>
+                        </li>
+                        <li className='sign-up__checklist-item'>
+                            <img className='sign-up__checklist-icon' src={icons.specialChar} alt='' />
+                            <span>One special character</span>
+                        </li>
+                        <li className='sign-up__checklist-item'>
+                            <img className='sign-up__checklist-icon' src={icons.number} alt='' />
+                            <span>One number</span>
+                        </li>
                     </ul>
                     <button className='sign-up__button' type='submit'>
                         Submit
