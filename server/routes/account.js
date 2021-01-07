3790e23854cb5f589296f9ff9e63e6d7287aee52
maggie388 <rodriguesmargarida@hotmail.com>
2021-01-07 16:51:52 -0500

    Add logic to /account/register route to handle when email already in database

--- server/routes/account.js
+++ server/routes/account.js
@@ -54,39 +54,70 @@ router.post('/login', (req, res) => {
 
 router.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    bcrypt.hash(password, saltRounds)
-        .then((hashedPassword) => {
-            new Users({
-                name,
-                email,
-                password: hashedPassword
-            })
-            .save()
-            .then((newUser) => {
-                const date = new Date();
-                date.setDate(date.getDate() + 1 );
-                crypto.randomBytes(20, (error, buf) => {
-                    const token = newUser.id + buf.toString('hex');
-                    newUser.save({
-                        active_token: token,
-                        active_expires: date
+    // Check if email is already in the database
+    Users
+        .where({ email })
+        .fetch({require: false})
+        .then((user) => {
+            // If email exists and is active, send an email to let user know and send response
+            if (user && user.attributes.active === 1) {
+                mailer({
+                    to: user.attributes.email,
+                    subject: ' You are Already Registered',
+                    html: `Hi ${user.attributes.name}, you already have an active account. PLease visit the home page to login.`
+                })
+                return res.status(200).json({ success: true });
+            }
+            // If email exists, is not active, and token is not expired, resend activation email and send response
+            if (user && user.attributes.active === 0 && user.attributes.active_expires > new Date()) {
+                const link = `${SERVER_URL}/account/active/${user.attributes.active_token}`;
+                mailer({
+                    to: user.attributes.email,
+                    subject: 'Activate your Account',
+                    html: 'Link: ' + link
+                })
+                return res.status(200).json({ success: true });
+            }
+            // If email exists, is not active, and token is expired, delete user and continue
+            if (user && user.attributes.active === 0 && user.attributes.active_expires < new Date()) {
+                user.destroy();
+            }
+            // Save new user and send activation email
+            bcrypt
+                .hash(password, saltRounds)
+                .then((hashedPassword) => {
+                    new Users({
+                        name,
+                        email,
+                        password: hashedPassword
                     })
-                    .then(updatedUser => {
-                        const link = `${SERVER_URL}/account/active/${updatedUser.attributes.active_token}`;
-                        mailer({
-                            to: updatedUser.attributes.email,
-                            subject: 'Welcome',
-                            html: 'Link: ' + link
-                        })
-                        res.status(200).send('new user saved and activation code sent');
-                    });
-                    
-                });
-            })
-            .catch(error => {
-                console.log(error);
-            })
-        })
+                    .save()
+                    .then((newUser) => {
+                        const date = new Date();
+                        date.setDate(date.getDate() + 1 );
+                        crypto.randomBytes(20, (error, buf) => {
+                            const token = newUser.id + buf.toString('hex');
+                            newUser.save({
+                                active_token: token,
+                                active_expires: date
+                            })
+                            .then(updatedUser => {
+                                const link = `${SERVER_URL}/account/active/${updatedUser.attributes.active_token}`;
+                                mailer({
+                                    to: updatedUser.attributes.email,
+                                    subject: 'Activate your Account',
+                                    html: 'Link: ' + link
+                                })
+                                res.status(200).send('new user saved and activation code sent');
+                            });
+                            
+                        });
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+                })
+        });
 });
 
 router.get('/active/:activeToken', (req, res) => {
@@ -113,12 +144,5 @@ router.get('/active/:activeToken', (req, res) => {
             })
         })
 });
-
-// const date = new Date();
-// console.log(typeof date);
-// console.log(date);
-// date.setDate(date.getDate() + 1 );
-// console.log(typeof date);
-// console.log(date)
 
 module.exports = router;
