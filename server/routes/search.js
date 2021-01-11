606785ac300387bc9b2650044aa9ac38fa8bbc6e
maggie388 <rodriguesmargarida@hotmail.com>
2021-01-11 13:47:30 -0500

    Refactor Search to look for the occurance of each work, not whole phrase

--- server/routes/search.js
+++ server/routes/search.js
@@ -3,17 +3,23 @@ const router = express.Router();
 const authorize = require('../utils/authorize');
 
 const Users = require('../models/users');
+const Recipes = require('../models/recipes');
 
 router.get('/', authorize, (req, res) => {
     const { query } = req.query;
+    const rawQueryIngredients = query.split(' ').map(element => `ingredients LIKE '%${element}%'`).join(' AND ');
+    const rawQueryTitle = query.split(' ').map(element => `title LIKE '%${element}%'`).join(' OR ');
     Users
         .where({ id: req.userId })
         .fetchAll({
             withRelated: [{
                 recipes: function(qb) {
                     qb
-                        .where('title', 'LIKE', `%${query}%`)
-                        .orWhere('ingredients', 'LIKE', `%${query}%`)
+                        .where(function() {
+                            this.where('user_id', req.userId)
+                            .andWhereRaw(rawQueryIngredients)
+                            .orWhereRaw(rawQueryTitle)
+                        })
                 }
             }]
         })
