37d896a0249f840e4067a864fd2ca174d893ac8e
maggie388 <rodriguesmargarida@hotmail.com>
2021-03-06 15:52:35 -0500

    Update Bookshelf/Knex config for production

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,10 +1,16 @@
 module.exports = {
-  client: 'mysql',
-  connection: {
-    host:'127.0.0.1',
-    user: 'root',
-    password: 'rootroot',
-    database: 'bookchef',
-    charset: 'utf8'
+  development: {
+    client: 'mysql',
+    connection: {
+      host:'127.0.0.1',
+      user: 'root',
+      password: 'rootroot',
+      database: 'bookchef',
+      charset: 'utf8'
+    }
+  },
+  production: {
+    client: "mysql",
+    connection: process.env.JAWSDB_URL
   }
 };
