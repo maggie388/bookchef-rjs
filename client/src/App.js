1502f990ac3547dba981eea948c5fe8b2dc94925
maggie388 <rodriguesmargarida@hotmail.com>
2021-03-07 13:28:48 -0500

    Move const below imports in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // COMPONENTS
 import Header from './components/Header/Header';
-const Profile = () => 'Profile page is under construction';
 
 // PAGES
 import Home from './pages/Home/Home';
@@ -14,6 +13,7 @@ import ActivationPage from './pages/ActivationPage/ActivationPage';
 import SignUp from './pages/SignUp/SignUp';
 
 // VARIABLES 
+const Profile = () => 'Profile page is under construction';
 export const API_URL = process.env.NODE_ENV === 'production'
     ? process.env.REACT_APP_API_URL_PRODUCTION
     : process.env.REACT_APP_API_URL_DEVELOPMENT;
