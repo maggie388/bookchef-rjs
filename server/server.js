f0f777eb4ca0a1186d036060b3af0314012d9b42
maggie388 <rodriguesmargarida@hotmail.com>
2021-03-07 11:12:53 -0500

    Update server/server.js for production

--- server/server.js
+++ server/server.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const app = express();
 
+// OTHER PACKAGE IMPORTS
+require('dotenv').config();
+const path = require('path');
+
+// ROUTERS
 const accountRouter = require('./routes/account');
 const recipesRouter = require('./routes/recipes');
 const notesRouter = require('./routes/notes');
 const uploadRouter = require('./routes/upload');
 const searchRouter = require('./routes/search');
 
-// OTHER PACKAGE IMPORTS
-require('dotenv').config();
-
-// MIDDLEWARE IMPORTS
+// MIDDLEWARE
 const cors = require('cors');
 const logger = require('morgan');
 
-// MIDDLEWARE FUNCTIONS
+// cors support
 app.use(cors());
-app.use(express.json());
+
+// init middleware
 app.use(logger('dev'));
+
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // ROUTERS
 app.use('/account', accountRouter);
@@ -28,8 +35,16 @@ app.use('/search', searchRouter);
 
 app.use(express.static('uploads/images'));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'));
+
+    app.get('*', (_req, res) => {
+        res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+    }); 
+}
+
 // START LISTENING
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
